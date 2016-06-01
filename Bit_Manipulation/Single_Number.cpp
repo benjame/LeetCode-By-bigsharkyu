73a20c457b8c31e2d8265f892bb5478b29350ef8
RobinWoodiz <ashakobe@gmail.com>
2016-06-01 18:07:59 +0800

    add some files.

--- Bit_Manipulation/Single_Number.cpp
+++ Bit_Manipulation/Single_Number.cpp
@@ -1,3 +1,9 @@
+/**
+ *  This code below was written by me.
+ *  but I don't use bit_manipulation.
+ *  T-O(n^2)
+ *  S-O(1)
+ */
 class Solution {
 public:
     /**
@@ -15,5 +21,29 @@ public:
             }
             if (flag) return i;
         }
+    }
+};
+
+/**
+ * This code was written by Others,which need to be learn by me;
+ * @author kamyu104
+ * T-O(n)
+ * S-O(1)
+ */
+
+class Solution {
+public:
+    /**
+     * @param A : An integer array
+     * @return : An integer
+     */
+    int singleNumberII(vector<int> &A) {
+        int one = 0, two = 0;
+        for (const auto& i : A) {
+            int new_one = (~i & one) | (i & ~one & ~two);
+            int new_two = (~i & two) | (i & one);
+            one = new_one, two = new_two;
+        }
+        return one;
     }
 };
